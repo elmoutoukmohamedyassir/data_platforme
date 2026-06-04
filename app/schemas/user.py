05b3974568e8432dafe1eb735567164742cc4f5c
elmoutoukmohamedyassir <elmoutoukmohamedyassir@gmail.com>
2026-06-04 12:14:02 +0100

    setup pydantic schema for users(input/output/Auth)

---
+++ app/schemas/user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from datetime import datetime
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    hashed_password: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+    created_at: datetime
+    is_active: bool
+
+class UserLogin(BaseModel):
+    email:str
+    password:str
+
+
+
+    
+    
