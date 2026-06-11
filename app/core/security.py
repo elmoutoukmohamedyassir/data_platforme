7442c2ea9b61adf2e4a6b34875983cf05863e275
elmoutoukmohamedyassir <elmoutoukmohamedyassir@gmail.com>
2026-06-11 11:13:51 +0100

    create jwt token, decode jwt token

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,9 @@
 import bcrypt
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+import os
+from dotenv import load_dotenv
 
 def hash_password(password: str) -> str:
     password_bytes = password.encode("utf-8")
@@ -11,4 +16,33 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
         hashed_password.encode("utf-8")
     )
 
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+# create jwt token 
+
+def create_access_token(data:dict,expires_delta : timedelta | None = None):
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+# -------------------------
+# DECODE JWT TOKEN
+# -------------------------
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload  # contains user_id (sub)
+    except JWTError:
+        return None
